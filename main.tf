03df6d0af13614b9717578f59eac2c202d71cbf5
Vaibhav Nimbalkar <63921438+vnnim@users.noreply.github.com>
2024-01-19 18:12:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "rfx-us-cl101pr-lvs-121260099-fs1"
-    prefix = "LVS-from-PR/atlantis"
+    bucket = "rfx-us-cl101pr-mav-122000099-fs1"
   }
   required_providers {
     google = {
