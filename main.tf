e88033bdc5a954df0fa5b6a7822bd64ff291a03b
Vaibhav Nimbalkar <63921438+vnnim@users.noreply.github.com>
2024-01-19 16:29:31 +0530

    removed bucket in main.tf

--- main.tf
+++ main.tf
@@ -34,12 +34,3 @@ resource "google_compute_instance" "default" {
 
   metadata_startup_script = "echo hi > /test.txt"
 }
-
-resource "google_storage_bucket" "auto-expire" {
-  project = "upheld-rain-404605"
-  name          = "no-public-access-bucket"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
