b1807bdf7bd0ed90bfc7d594e3cc4d5138e45b7d
Vaibhav Nimbalkar <63921438+vnnim@users.noreply.github.com>
2024-01-19 17:51:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,41 @@
-resource "google_compute_instance" "default" {
-  project = "upheld-rain-404605"
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
+terraform {
+  backend "gcs" {
+    bucket = "rfx-us-cl101pr-lvs-121260099-fs1"
+    prefix = "LVS-from-PR/atlantis"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.40.0"
+    }
+  }
+}
 
-  tags = ["foo", "bar"]
+provider "google" {
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = "e2-micro"
+  
+  zone         = var.zone
+  tags = ["dev", "engineering"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20221014"
       labels = {
-        my_label = "value"
+        env = "dev"
       }
     }
   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
-
   network_interface {
     network = "default"
-
     access_config {
       // Ephemeral public IP
     }
   }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
 }
-
